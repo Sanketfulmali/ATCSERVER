825e1dbdd2ae4c02e01b4fd099fdf4b14cd315c5
Sanketfulmali <114637873+Sanketfulmali@users.noreply.github.com>
2023-12-10 11:30:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors'); // Import the cors middleware
 
 const app = express();
 const port = 3001; // Use the port you prefer
 
+app.use(cors()); // Enable CORS for all routes
 app.use(bodyParser.json());
 
 app.post('/receive-text', (req, res) => {
+  console.log("GET HELO REQ");
   const { text } = req.body;
   console.log('Received text:', text);
 
